21d7cfb5ec5f144358741c1f3a60acaa4709073f
anilyildiz <yildiz@mbd.rwth-aachen.de>
2024-10-23 12:37:55 +0200

    Soil class is adapted

--- src/pyrootmemo/materials.py
+++ src/pyrootmemo/materials.py
@@ -8,10 +8,14 @@ ROOT_PARAMETERS = {
     "tensile_strength": (float | int),
 }
 SOIL_PARAMETERS = {
-    "type": str,
+    "name": str,
+    "uscs_class": str,
+    "usda_class": str,
     "cohesion": (float | int),
     "friction_angle": (float | int),
-    "unit_weight": (float | int),
+    "unit_weight_bulk": (float | int),
+    "unit_weight_dry": (float | int),
+    "unit_weight_saturated": (float | int),
     "water_content": (float | int),
 }
 
@@ -44,34 +48,7 @@ class MultipleRoots(Roots):
 
 
 class Soil:
-    def __init__(
-        self, type: str, friction_angle=None, unit_weight: dict = None, cohesion=None
-    ):
-        """
-        Initialises Soil class
-
-        Args:
-            type (str): _description_
-            friction_angle (int or float, optional): Friction angle of the soil in degrees. Defaults to None.
-            unit_weight (dict, optional): Unit weight of soil in kN/m^3. Allowed keys are saturated, dry, bulk. Defaults to None.
-            cohesion (int or float, optional): Cohesion of soil in kPa. Defaults to None.
-        """
-        self.type = type
-        self.friction_angle = friction_angle
-        self.cohesion = cohesion
-        unit_weight_keys = ["saturated", "dry", "bulk"]
-
-        try:
-            for k, v in unit_weight.items():
-                if k in unit_weight_keys:
-                    setattr(self, f"{k}_unit_weight", v)
-                    # TODO: check against int or float
-                    # TODO: Users should not be able to multiple entries with same key
-                else:
-                    raise KeyError(
-                        f"{k} should be one of the following: {unit_weight_keys}"
-                    )
-        except TypeError:
-            print("TypeError: Unit weight should be of type dict")
-        except AttributeError:
-            print("AttributeError: Unit weight should be of type dict")
+    def __init__(self, **kwargs):
+        if check_kwargs(arguments=kwargs, parameters=SOIL_PARAMETERS):
+            for k, v in kwargs.items():
+                setattr(self, k, v)
