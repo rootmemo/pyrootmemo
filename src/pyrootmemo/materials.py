0338dd03267d1a39fd70e44d1a58d0cbb29bfa7c
anilyildiz <yildiz@mbd.rwth-aachen.de>
2024-11-19 11:22:45 +0100

    units are implemented

--- src/pyrootmemo/materials.py
+++ src/pyrootmemo/materials.py
@@ -1,12 +1,17 @@
 import numpy as np
-from pyrootmemo.tools.checks import check_kwargs
+from pyrootmemo.tools.checks import check_kwargs, is_namedtuple
+from pint import UnitRegistry, DimensionalityError
+from collections import namedtuple
+
+ureg = UnitRegistry()
+Parameter = namedtuple("parameter", "value unit")
 
 ROOT_PARAMETERS = {
-    "species": str,
-    "elastic_modulus": (float | int),
-    "diameter": (float | int),
-    "tensile_strength": (float | int),
+    "elastic_modulus": {"type": (float | int), "unit": ureg("MPa")},
+    "diameter": {"type": (float | int), "unit": ureg("m")},
+    "tensile_strength": {"type": (float | int), "unit": ureg("MPa")},
 }
+
 SOIL_PARAMETERS = {
     "name": str,
     "uscs_class": str,
@@ -22,15 +27,38 @@ SOIL_PARAMETERS = {
 
 class Roots:
     def __init__(self, **kwargs):
-        if check_kwargs(arguments=kwargs, parameters=ROOT_PARAMETERS):
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+        for k, v in kwargs.items():
+            if k not in ROOT_PARAMETERS.keys():
+                raise ValueError(
+                    f"Undefined parameter. Choose one of the following: {ROOT_PARAMETERS.keys()}"
+                )
+            if not is_namedtuple(v):
+                raise TypeError("Parameter should be of type Parameter(value, unit)")
+            if not isinstance(v.value, (ROOT_PARAMETERS[k]["type"] | list)):
+                raise TypeError(
+                    f"Value should be of type {ROOT_PARAMETERS[k]["type"]} or a list"
+                )
+            if not isinstance(v.unit, str):
+                raise TypeError("Unit should be entered as a string")
+            if not ureg(v.unit).check(ROOT_PARAMETERS[k]["unit"].dimensionality):
+                raise DimensionalityError(
+                    units1=v.unit, units2=ROOT_PARAMETERS[k]["unit"]
+                )
+            if isinstance(v.value, list):
+                if not all(
+                    [isinstance(entry, ROOT_PARAMETERS[k]["type"]) for entry in v.value]
+                ):
+                    raise TypeError(
+                        f"{k} should only be of type {ROOT_PARAMETERS[k]["type"]} in a list"
+                    )
+
+            setattr(self, k, v.value * ureg(v.unit))
 
     def calc_xsection(self) -> float:
-        return np.pi * np.array(self.diameter) ** 2 / 4
+        return np.pi * np.array(self.diameter.magnitude) ** 2 / 4
 
     def calc_circumference(self) -> float:
-        return np.pi * np.array(self.diameter)
+        return np.pi * np.array(self.diameter.magnitude)
 
 
 class SingleRoot(Roots):
